ced99d8748baa1ce67b71c34cfa86bfb3268b7bf
ARK <120788835+cleanmountain@users.noreply.github.com>
2023-01-27 21:17:16 +0100

    Redid pretty much everything
    main takeaways
    - Prettier output
    - Now keeps all values mapped to the same key instead of having several lines for each key+value pair
    - Bugfixes

--- medianajones.py
+++ medianajones.py
@@ -3,91 +3,111 @@ import json
 
 
 # MODIFY THESE PARAMETERS
-ALL_FILES = []  # e.g. ["swing-video.mp4", "fireworks.mp4", "berlin-2088.mp4"]
+ALL_FILES = ["swing-rain-new-audio.mp4", "tree-rain.mp4"]
 HIDE_NOT_IN_COMMONS = False
 
 
-def load_video(file):
-    media_info = MediaInfo.parse(file, output="JSON")
+class bcolors:
+    HEADER = '\033[95m'
+    ENDC = '\033[0m'
+    OKGREEN = '\033[92m'
+    FAIL = '\033[91m'
     
+    #not in use
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    WARNING = '\033[93m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def load_video(file):
+    USUAL_TRACKS = ["general", "video", "audio"]
+    media_info = MediaInfo.parse(file, output="JSON")
     data = json.loads(media_info)["media"]["track"]
     
-    general = [f"{key}: {value}" for [key, value] in data[0].items()]
-    video = [f"{key}: {value}" for [key, value] in data[1].items()]
-    audio = [f"{key}: {value}" for [key, value] in data[2].items()]
+    tracks_data = []
+
+    for track in data:
+        track_data = {}
+        track_type = track["@type"].lower().strip()
+        track_type_id = track["StreamKindID"]
+        
+        if track_type not in USUAL_TRACKS:
+            print(f"Unusual track type {track_type} found")
+        
+        media_info_for_track = {}
+
+        for mediainfo in track:
+            media_info_for_track[mediainfo] = track[mediainfo]
+
+        track_data["type"] = track_type  # e.g. video
+        track_data["track_type_id"] = track_type_id  # e.g. 0
+        track_data["id"] = track_type + "_" + track_type_id  # e.g. video_0
+        track_data["data"] = media_info_for_track  # all mediainfo for the track
+
+        tracks_data.append(track_data)    
+
     
-    return general, video, audio
-
-
-def find_commons(number_of_files, general, video, audio):
-    
-    # font colors
-    okblue = "\033[94m"
-    failred = "\033[91m"
-    
-    if HIDE_NOT_IN_COMMONS: 
-        # ignore not-in-commons
-        general_in_common = [okblue + info for info in general if general[info] == number_of_files]
-        video_in_common = [okblue + info for info in video if video[info] == number_of_files]
-        audio_in_common = [okblue + info for info in audio if audio[info] == number_of_files]
-    else: 
-        # color in-commons blue and not-in-commons red
-        general_in_common = [okblue + info if general[info] == number_of_files else failred + info for info in general]
-        video_in_common = [okblue + info if video[info] == number_of_files else failred + info for info in video]
-        audio_in_common = [okblue + info if audio[info] == number_of_files else failred + info for info in audio]
-
-    print_result(general_in_common, video_in_common, audio_in_common)
-
-
-def print_result(general, video, audio):
-    # font color
-    header = "\033[95m"
-
-    print("\n")
-    print(header + ">>>> GENERAL <<<<")
-    for line in general:
-        print(line)
-    print("\n\n")
-
-    print(header + ">>>> VIDEO <<<<")
-    for line in video:
-        print(line)
-    print("\n\n")
-
-    print(header + ">>>> AUDIO <<<<")
-    for line in audio:
-        print(line)
+    return tracks_data
 
 
 def count_occurences():
-    general_info_counter = {}
-    video_info_counter = {}
-    audio_info_counter = {}
-
-    NUM_OF_FILES = len(ALL_FILES)
+    all_videos_data = {}
+    longest_key_word = 0
 
     for file in ALL_FILES:
-        general, video, audio = load_video(file)
+        video_tracks = load_video(file)  # e.g. [{'type': 'audio', 'track_type_id': '1', 'id': 'audio_1', 'data': {'@type': 'Audio', '@typeorder': '2',]
         
-        for info in general:
-            if info in general_info_counter:
-                general_info_counter[info] += 1
-            else:
-                general_info_counter[info] = 1
-        
-        for info in video:
-            if info in video_info_counter:
-                video_info_counter[info] += 1
-            else:
-                video_info_counter[info] = 1
+        for track in video_tracks:
+            track_type = track["type"]
+            for key in track["data"]:
 
-        for info in audio:
-            if info in audio_info_counter:
-                audio_info_counter[info] += 1
-            else:
-                audio_info_counter[info] = 1
+                if key == "extra":  # can't decide what to do with this rn (it's a dictionary of extra details)
+                    continue
+                
+                if len(key) > longest_key_word:
+                    longest_key_word = len(key)
+
+                if not track_type in all_videos_data:
+                    all_videos_data[track_type] = {}
+                
+                track_type_data = all_videos_data[track_type]
+                
+                if not key in track_type_data:
+                    """" INITIALIZE DICT STRUCTURE """
+                    track_type_data[key] = {"tracks_with_key": 0, "videos": set(), "entries": set()}
+
+                if not file in track_type_data[key]["videos"]:
+                    track_type_data[key]["videos"].add(file)
+                
+                track_type_data[key]["tracks_with_key"] += 1
+                track_type_data[key]["entries"].add(track["data"][key])
+
+    print_result(all_videos_data, longest_key_word)
+
+
+def print_result(all_videos_data, longest_key_word):
+    # Criteria for being a common: len(entries) == 1 && len(videos) == len(ALL_FILES)
+    # ...since it means that all videos have added their value to the respective key, and that all the values were identical (entries is a set)
     
-    find_commons(NUM_OF_FILES, general_info_counter, video_info_counter, audio_info_counter)
+    for track_type in all_videos_data:
 
+        """PRINT HEADER"""
+        print(f"\n\n{bcolors.HEADER}{(track_type.upper() + ' • ')*4 + track_type.upper()}{bcolors.ENDC}")
+        
+        for key in all_videos_data[track_type]:
+            
+            entries = all_videos_data[track_type][key]["entries"]
+            videos = all_videos_data[track_type][key]["videos"]
+            
+            
+            if len(entries) == 1 and len(videos) == len(ALL_FILES):
+                """PRINT IN COMMONS"""
+                print(bcolors.OKGREEN + f"{key : <{longest_key_word}s}: {entries}")
+            elif not HIDE_NOT_IN_COMMONS:
+                """PRINT NOT IN COMMONS"""
+                print(bcolors.FAIL + f"{key : <{longest_key_word}s}: {entries}")
+        
 
 count_occurences()
